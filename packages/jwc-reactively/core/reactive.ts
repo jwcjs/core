63e74c68233987d8d4179432541e36e156c2c4e9
wibus-wee <1596355173@qq.com>
2022-12-15 23:21:39 +0800

    feat(jwc-reactively): base reactive data handler

---
+++ packages/jwc-reactively/core/reactive.ts
@@ -0,0 +1,34 @@
+/**
+ * We use getter and setter to make sure that the value is always
+ * up to date. This is important because we want to be able to
+ * use the value in a template.
+ * 
+ * We use Reflect.get and Reflect.set to make sure that we can
+ * use the value in a template.
+ */
+export function reactive<T extends Object>(target: T, key: string): T {
+  return new Proxy<T>(target, {
+    get(target, key) {
+      return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+      Reflect.set(target, key, value);
+      return true;
+    }
+  });
+}
+
+// Usage
+// const state = reactive({ count: 0 }, 'count');
+// state.count = 1;
+// console.log(state.count); // 1
+
+// Use in web component
+// class MyElement extends HTMLElement {
+//   constructor() {
+//     super();
+//     this.state = reactive({ count: 0 }, 'count');
+//     this.state.count = 1;
+//     console.log(this.state.count); // 1
+//   }
+// }
