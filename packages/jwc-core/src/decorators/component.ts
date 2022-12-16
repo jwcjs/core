b827d9a005ec2d7392aa00cfe1a033e2bdfc0e8e
wibus-wee <1596355173@qq.com>
2022-12-16 16:56:26 +0800

    fix(jwc-core): component decorators doesn't return a function

--- packages/jwc-core/src/decorators/component.ts
+++ packages/jwc-core/src/decorators/component.ts
@@ -1,11 +1,13 @@
 import { CustomElementProps } from "../types";
 
-export function Component(_class: any, options: CustomElementProps) {
-  // set the default value for the isMounted option
-  if (options.isMounted === undefined) options.isMounted = true;
-  if (customElements.get(options.name)) {
-    console.warn(`The component ${options.name} already exists.`);
-  }
-  _class.$options = options; // add the options to the class
-  customElements.define(options.name, _class, options.options || {});
+export function Component(options: CustomElementProps) {
+  return function (_class: any) {
+    // set the default value for the isMounted option
+    if (options.isMounted === undefined) options.isMounted = true;
+    if (customElements.get(options.name)) {
+      console.warn(`The component ${options.name} already exists.`);
+    }
+    _class.$options = options; // add the options to the class
+    customElements.define(options.name, _class, options.options || {});
+  };
 }
