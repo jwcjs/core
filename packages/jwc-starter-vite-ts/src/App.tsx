2865246449adaaf3588312e828811ebbfcd4c9cb
wibus-wee <1596355173@qq.com>
2022-12-17 12:35:57 +0800

    chore(jwc-starter-vite-ts): create events

--- packages/jwc-starter-vite-ts/src/App.tsx
+++ packages/jwc-starter-vite-ts/src/App.tsx
@@ -1,20 +1,33 @@
-import { JwcComponent, Component } from "@jwcjs/core";
+import { JwcComponent, Component, Prop, Event } from "@jwcjs/core";
 import { h } from "@jwcjs/runtime";
-import style from './App.css?inline'
+// import style from './App.css'
 
 @Component({
   name: "app-element",
-  css: style
+  // css: style
 })
 export class App extends JwcComponent {
   constructor() {
     super();
   }
 
+  @Prop({ default: "Hi" })
+  public msg: string = "Hi";
+
+  @Event("click")
+  public onClick() {
+    console.log("clicked");
+    this.msg = "Hello";
+    console.log(this.msg);
+  }
+
   public override render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1
+          onClick={this.onClick}
+        >Hello World</h1>
+        <p>{this.msg}</p>
       </div>
     )
   }
