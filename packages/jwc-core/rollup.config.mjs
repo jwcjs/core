0a20d62c35b433109e7fedcd13bc0662cbbf92a4
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-18 22:06:07 +0800

    feat: core bundle

---
+++ packages/jwc-core/rollup.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "rollup";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import { minify } from "rollup-plugin-esbuild";
+
+export default defineConfig({
+	input: "./index.ts",
+	output: {
+		file: "./dist/index.js",
+	},
+	plugins: [
+		nodeResolve(),
+		typescript({ exclude: "../jwc-starter-vite-ts/**" }),
+		minify(),
+	],
+});
