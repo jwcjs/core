b41a115db92b012e5522aafff0d39f09e871c3c4
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-19 12:01:45 +0800

    feat: starter vite config

--- packages/jwc-starter-vite-ts/vite.config.ts
+++ packages/jwc-starter-vite-ts/vite.config.ts
@@ -1,14 +1,2 @@
 import { defineConfig } from "vite";
-export default defineConfig({
-	// 把 CSS 移动到 dist 目录
-	build: {
-		lib: {
-			entry: ["src/index.ts"],
-			formats: ["es"],
-			fileName: (format) => `index.${format}.js`,
-		},
-		rollupOptions: {
-			external: [""],
-		},
-	},
-});
+export default defineConfig({});
